39bb1d0f4552f4ca73c849c437dc3f1943b90223
uttampreet-dev <uttampreetkaur02@gmail.com>
2026-05-10 23:43:20 +0530

    cleanup

--- dashboard/test-axios.mjs
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-axios.post('https://shescam.onrender.com/api/analyze', { message: 'test', city: 'delhi' })
-  .then(r => console.log("OK", r.status, r.data))
-  .catch(e => console.log("ERR", e.message, e.response?.status, e.response?.data));
