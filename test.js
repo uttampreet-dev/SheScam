840a65fa334af642c914bcd4a5344dfa8ca08788
uttampreet-dev <uttampreetkaur02@gmail.com>
2026-05-10 23:58:06 +0530

    remove test files

--- test.js
+++
@@ -1,4 +0,0 @@
-const axios = require("axios");
-axios.post("https://shescam.onrender.com/api/analyze", { message: "test", city: "test" })
-  .then(res => console.log("OK", res.status))
-  .catch(err => console.log("ERR", err.response?.status, err.response?.data));
